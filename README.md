14f1acf6f281db3f5ef3d7e49d3c18c5bbbc5b1f
johnwastle <johnwastle@yahoo.co.uk>
2013-09-12 22:07:07 +0100

    Minor code tidying. Add README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 lastfm-dataexport
 =================
+Execute LastFmExportTracksToTabDelimitedFile to export last.fm tracks to a tab-delimited text file.
+
+This code example writes:
+    Artist
+    Track name
+    Album name
+    Scrobble timestamp
+
+Assuming you have a MySQL table as follows:
+
+    CREATE TABLE `lastfm`.`tracks` (
+    `artist` varchar(255)  NOT NULL,
+    `track` varchar(255)  NOT NULL,
+    `album` varchar(255)  NOT NULL,
+    `ts` timestamp  NOT NULL,
+           KEY artist (artist),
+           KEY track (track),
+           KEY album (album),
+           KEY ts (ts)
+    )
+
+You can load data from the 'tracks.tsv' file with
+    LOAD DATA LOCAL INFILE 'tracks.tsv' INTO TABLE tracks FIELDS TERMINATED BY '\t' ENCLOSED BY '"' ;
+
+
+##Known issues
+
+Track names that include backslashes need to be escaped.
+'/ / M \ \' by Health is the only example I can find though.
+http://www.last.fm/music/Health/_/%2F%2FM%5C%5C
+
+
+select * from tracks where track like '/ / M%';
+update tracks set track='/ / M \\ \\', album= 'HEALTH' where track like '/ / M%';
--- src/main/java/com/johnwastle/lastfm/LastFmExportTracksToTabDelimitedFile.java
+++ src/main/java/com/johnwastle/lastfm/LastFmExportTracksToTabDelimitedFile.java
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
 
-public class LastFmExport {
+public class LastFmExportTracksToTabDelimitedFile {
 
     public static String TAB = "\t";
     public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static String DEFAULT_ACCOUNT = "johnwastle";
-    public static String API_KEY = "3b0c397650d6804f204708e08ed0b26b";
+    public static String API_KEY = "";        // get one of these from http://www.last.fm/api/account/create
 
     private static ObjectMapper mapper = new ObjectMapper();
     private static HttpRequestExecutor httpRequestExecutor = new HttpRequestExecutor();
@@ -32,29 +32,35 @@ public class LastFmExport {
         if (args.length>1) { startPage = Integer.parseInt( args[1] ); }
         if (args.length>2) { endPage = Integer.parseInt( args[2] ); }
 
-        (new LastFmExport() ).processAccount(account, startPage, endPage);
+        (new LastFmExportTracksToTabDelimitedFile() ).processAccount(account, startPage, endPage);
 
     }
 
     public synchronized void processAccount(String accountName, int startPage, int endPage) throws Exception{
 
-        PrintWriter writer = new PrintWriter("tracks.txt", "UTF-8");
+        PrintWriter writer = new PrintWriter("tracks.tsv", "UTF-8");
 
         for (int i=startPage; i <= endPage; i++) {
 
             String url = "http://ws.audioscrobbler.com/2.0/?format=json&method=user.getRecentTracks&limit=200&user=" + accountName + "&api_key=" + API_KEY+ "&page=" + i;
-System.out.println(url);
+            System.out.println(url);
 
             String response = httpRequestExecutor.getURLContents(url);
 
-            //wait(200);    // throttle calls to last.fm API
+            //wait(200);    // throttle calls to last.fm API, if they execute too quickly
 
+            // Jackson "Raw" Data Binding:
+            // http://wiki.fasterxml.com/JacksonInFiveMinutes#A.22Raw.22_Data_Binding_Example
+            // could use
+            // ArrayList tracks0 = getTrackListFromResponseUsingJacksonRawDataBinding(response);
+
+            // Full Data Binding (POJO):
+            // http://wiki.fasterxml.com/JacksonInFiveMinutes#Full_Data_Binding_.28POJO.29_Example
             Tracks tracks = mapper.readValue(response, Tracks.class);
             ArrayList<Track> tracksList = tracks.getRecenttracks().getTrack();
 
             for (int j=0; j < tracksList.size(); j++) {
                 String trackOutput = formatTrackOutput(tracksList.get(j));
-                //System.out.println(trackOutput);
                 writer.println(trackOutput);
             }
         }
@@ -74,10 +80,8 @@ System.out.println(url);
         return trackDetail.toString();
     }
 
-    //  example usage:
-    //  ArrayList tracks0 = getTrackListFromResponseUsingSimpleDataBinding(response);
 
-    private ArrayList getTrackListFromResponseUsingSimpleDataBinding(String response) throws IOException {
+    private ArrayList getTrackListFromResponseUsingJacksonRawDataBinding(String response) throws IOException {
         Map<?,?> rootAsMap = mapper.readValue(response, Map.class);
         Map recentTracks = (Map)rootAsMap.get("recenttracks");
         return (ArrayList)recentTracks.get("track");
